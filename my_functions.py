cd67a48b36017f59e54cb3f94cbd23230246ecbf
helsing85 <helsing85@gmail.com>
2022-06-27 20:05:44 +0200

    OtoDom - Dodanie wyjątków związanych z obsługą pliku arkusza kalkulacyjnego. Dodanie kolumny z datą do arkusza.

--- my_functions.py
+++ my_functions.py
@@ -2,23 +2,27 @@ def readLinksFromCSV(filepath):
     import pandas
 
     dane = pandas.read_csv(filepath, sep=';', encoding='windows-1250')
-    
-    dane_list = [{'Nazwa' : row["Nazwa"], 'Link' : row["Link"]} for (index, row) in dane.iterrows()]
 
-    #print(dane_list)
+    dane_list = [{'Nazwa': row["Nazwa"], 'Link': row["Link"]}
+                 for (index, row) in dane.iterrows()]
+
+    # print(dane_list)
 
     return dane_list
+
 
 def readLinksFromExcel(filepath):
     import pandas
 
     dane = pandas.read_excel(filepath, sheet_name=0)
- 
-    dane_list = [{'Nazwa' : row["Nazwa"], 'Link' : row["Link"]} for (index, row) in dane.iterrows()]
 
-    #print(dane_list)
+    dane_list = [{'Nazwa': row["Nazwa"], 'Link': row["Link"]}
+                 for (index, row) in dane.iterrows()]
+
+    # print(dane_list)
 
     return dane_list
+
 
 def getCurrentTime():
     import datetime
--- oto-dom.py
+++ oto-dom.py
@@ -12,25 +12,29 @@ import pandas
 PLIK_DANE = "oto-dom.xlsx"
 PLIK_LOG = "oto-dom.log"
 
+
 def readDataFromSite(url):
     response = requests.get(url)
     webpage = response.text
     soup = BeautifulSoup(webpage, "html.parser")
-    
-	#Zapis kodu strony do pliku
+
+    # Zapis kodu strony do pliku
     # with open("html.html", "w", encoding="utf-8") as f:
     #     f.write(soup.prettify())
 
-    #Tytuł
+    # Tytuł
     elems = soup.select_one("header.css-1s2plby.eu6swcv26")
     if elems is not None:
         tytul = elems.select_one("h1").get_text().strip()
-        cena = elems.select_one("strong.css-8qi9av.eu6swcv19").get_text().strip()
-        lok = elems.select_one("a.e1nbpvi60.css-1kforri.e1enecw71").get_text().strip()
-        cena_m2 = elems.select_one("div.css-1p44dor.eu6swcv16").get_text().strip()
-    
-        #Szczegóły ogłoszenia
-        #Spacja oznacza że znacznik jest gdzieś niżej
+        cena = elems.select_one(
+            "strong.css-8qi9av.eu6swcv19").get_text().strip()
+        lok = elems.select_one(
+            "a.e1nbpvi60.css-1kforri.e1enecw71").get_text().strip()
+        cena_m2 = elems.select_one(
+            "div.css-1p44dor.eu6swcv16").get_text().strip()
+
+        # Szczegóły ogłoszenia
+        # Spacja oznacza że znacznik jest gdzieś niżej
         elems = soup.select("div.css-wj4wb2.emxfhao1 div.css-1qzszy5.estckra8")
         pow = elems[1].get_text().strip()
         wlasnosc = elems[3].get_text().strip()
@@ -40,14 +44,15 @@ def readDataFromSite(url):
         balkon = elems[11].get_text().strip()
         garaz = elems[15].get_text().strip()
 
-        #Opis
+        # Opis
         # znak > oznacza że znacznik jest bezpośrednio niżej
         elems = soup.select_one("section.css-3hljba.e1r1048u3 > div")
         opis = elems.get_text().strip()
-        
-        #Informacje dodatkowe
-        #Spacja oznacza że znacznik jest gdzieś niżej
-        elems = soup.select("div.css-1l1r91c.emxfhao1 div.css-f45csg.estckra9 div.estckra8")
+
+        # Informacje dodatkowe
+        # Spacja oznacza że znacznik jest gdzieś niżej
+        elems = soup.select(
+            "div.css-1l1r91c.emxfhao1 div.css-f45csg.estckra9 div.estckra8")
         rynek = elems[1].get_text().strip()
         ogloszenie = elems[3].get_text().strip()
         winda = elems[13].get_text().strip()
@@ -57,51 +62,66 @@ def readDataFromSite(url):
         # print(rynek, ogloszenie, winda)
         # print(opis)
 
-        dane_strony = [{'Data' : my_functions.getCurrentTime(),
-                        'Tytuł': tytul, 'Cena': cena, 'Cena/m²': cena_m2, 'Powierzchnia' : pow, 'Lokalizacja' : lok,
-                        'Typ ogłoszenia' : ogloszenie, 'Własność' : wlasnosc, 'Liczba pokoi' : pokoje, 'Wykończenie' : wykonczenie,
-                        'Piętro' : pietro, 'Balkon' : balkon, 'Garaż' : garaz, 'Winda' : winda, 'Rynek' : rynek, 'Opis' : opis }]
+        dane_strony = [{'Data': my_functions.getCurrentTime(),
+                        'Tytuł': tytul, 'Cena': cena, 'Cena/m²': cena_m2, 'Powierzchnia': pow, 'Lokalizacja': lok,
+                        'Typ ogłoszenia': ogloszenie, 'Własność': wlasnosc, 'Liczba pokoi': pokoje, 'Wykończenie': wykonczenie,
+                        'Piętro': pietro, 'Balkon': balkon, 'Garaż': garaz, 'Winda': winda, 'Rynek': rynek, 'Opis': opis}]
 
-        df = pandas.DataFrame(dane_strony) 
+        df = pandas.DataFrame(dane_strony)
 
-        #print(df)
-    
+        # print(df)
+
         return df
+
 
 def logowanie(tekst, plik):
     print(tekst, file=plik)
     print(tekst)
 
+
 def main():
-    
+
     plik_logow = open(PLIK_LOG, 'w')
 
-    #dane = dane_testowe
-    dane = my_functions.readLinksFromExcel(PLIK_DANE)
-    
-    if len(dane)>0:
+    try:
+        #dane = dane_testowe
+        dane = my_functions.readLinksFromExcel(PLIK_DANE)
+    except FileNotFoundError:
+        logowanie('Brak pliku danych: ' + PLIK_DANE, plik_logow)
+        dane = []
+
+    if len(dane) > 0:
         xl = pandas.ExcelFile(PLIK_DANE)
         arkusze = xl.sheet_names
-        with pandas.ExcelWriter(PLIK_DANE, mode="a", if_sheet_exists="overlay") as writer:
-            for dom in dane:
-                nazwa_oferty = dom["Nazwa"]
-                logowanie(nazwa_oferty, plik_logow)
-                strona_df = readDataFromSite(dom["Link"])
-                if strona_df is None:
-                    logowanie('--- Brak oferty', plik_logow)
-                else:
-                    if nazwa_oferty in arkusze:
-                        plik_df = xl.parse(nazwa_oferty)
-                        polaczony_df = pandas.concat([strona_df, plik_df])
-                        polaczony_df.to_excel(writer, sheet_name=nazwa_oferty, index=False) 
+        try:
+            with pandas.ExcelWriter(PLIK_DANE, mode="a", if_sheet_exists="overlay") as writer:
+                for dom in dane:
+                    nazwa_oferty = dom["Nazwa"]
+                    logowanie(nazwa_oferty, plik_logow)
+                    strona_df = readDataFromSite(dom["Link"])
+                    if strona_df is None:
+                        logowanie(
+                            '--- Oferta nie istnieje na podanej stronie', plik_logow)
                     else:
-                        strona_df.to_excel(writer, sheet_name=nazwa_oferty, index=False) 
+                        if nazwa_oferty in arkusze:
+                            plik_df = xl.parse(nazwa_oferty)
+                            polaczony_df = pandas.concat([strona_df, plik_df])
+                            polaczony_df.to_excel(
+                                writer, sheet_name=nazwa_oferty, index=False)
+                        else:
+                            strona_df.to_excel(
+                                writer, sheet_name=nazwa_oferty, index=False)
+        except PermissionError:
+            logowanie('Błąd dostępu do pliku z danymi:' +
+                      PLIK_DANE, plik_logow)
+        except Exception as e:  # work on python 3.x
+            logowanie('Inny błąd: ' + str(e), plik_logow)
     else:
-        logowanie('Brak danych', plik_logow)
+        logowanie('Brak ofert.', plik_logow)
 
     logowanie('--KONIEC--', plik_logow)
     plik_logow.close()
-    
+
 
 if __name__ == '__main__':
     main()
