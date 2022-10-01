05ff602abb68840866ecfdfbff834510076c223e
helsing85 <helsing85@gmail.com>
2022-10-01 13:01:44 +0200

    Chromedriver bez GUI

--- webdriver.py
+++ webdriver.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
 def chromedriver():
     chrome_driver_path = "webdriver/chromedriver.exe"
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
     s = Service(chrome_driver_path)
 
-    return webdriver.Chrome(service=s)
+    return webdriver.Chrome(service=s, options=chrome_options)
