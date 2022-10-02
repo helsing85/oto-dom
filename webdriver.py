847b1b93ab4ff67bbe2c6049e90e256ee1ae9b2a
helsing85 <helsing85@gmail.com>
2022-10-02 16:01:55 +0200

    Przygotowanie kodu do obsługi przeglądarki Firefox

--- webdriver.py
+++ webdriver.py
@@ -1,16 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 
 
 def chromedriver():
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+
     chrome_driver_path = "webdriver/chromedriver.exe"
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
 
     s = Service(chrome_driver_path)
 
@@ -18,6 +18,9 @@ def chromedriver():
 
 
 def edgedriver():
+    from selenium.webdriver.edge.options import Options
+    from selenium.webdriver.edge.service import Service
+
     edge_driver_path = "webdriver/msedgedriver.exe"
 
     edge_options = Options()
@@ -27,3 +30,19 @@ def edgedriver():
     s = Service(edge_driver_path)
 
     return webdriver.Edge(service=s, options=edge_options)
+
+
+def firefoxdriver():
+    from os import path
+    from selenium.webdriver.firefox.options import Options
+    from selenium.webdriver.firefox.service import Service
+
+    ff_driver_path = "webdriver/geckodriver.exe"
+
+    ff_options = Options()
+    ff_options.add_argument("--headless")
+    ff_options.add_argument("--disable-gpu")
+
+    s = Service(ff_driver_path, log_path=path.devnull)
+
+    return webdriver.Edge(service=s, options=ff_options)
