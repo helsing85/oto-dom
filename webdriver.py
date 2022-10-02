6352ef566e31dd0b5848dc6c97452e5e610c5b34
helsing85 <helsing85@gmail.com>
2022-10-02 15:36:25 +0200

    Przygotowanie kodu do obsługi przeglądarki MS Edge

--- webdriver.py
+++ webdriver.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 
 
@@ -13,3 +15,15 @@ def chromedriver():
     s = Service(chrome_driver_path)
 
     return webdriver.Chrome(service=s, options=chrome_options)
+
+
+def edgedriver():
+    edge_driver_path = "webdriver/msedgedriver.exe"
+
+    edge_options = Options()
+    edge_options.add_argument("headless")
+    edge_options.add_argument("disable-gpu")
+
+    s = Service(edge_driver_path)
+
+    return webdriver.Edge(service=s, options=edge_options)
