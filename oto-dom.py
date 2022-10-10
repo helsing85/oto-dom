c26af69c416d984addd4977bbaf7a4415e9ca82f
helsing85 <helsing85@gmail.com>
2022-10-10 20:16:23 +0200

    Poprawienie odczytu braku ogłoszenia.

--- oto-dom.py
+++ oto-dom.py
@@ -25,10 +25,13 @@ def readDataFromSiteSelenium(url):
     # Tytuł
     elem = DRV.find_elements(by=By.TAG_NAME, value="header")
     if len(elem) > 0:
-        tytul = elem[0].find_element(by=By.TAG_NAME, value="h1").text
-        cena = elem[0].find_element(by=By.TAG_NAME, value="strong").text
-        lok = elem[0].find_element(by=By.XPATH, value="./div[3]").text
-        cena_m2 = elem[0].find_element(by=By.XPATH, value="./div[4]").text
+        try:
+            tytul = elem[0].find_element(by=By.TAG_NAME, value="h1").text
+            cena = elem[0].find_element(by=By.TAG_NAME, value="strong").text
+            lok = elem[0].find_element(by=By.XPATH, value="./div[3]").text
+            cena_m2 = elem[0].find_element(by=By.XPATH, value="./div[4]").text
+        except NoSuchElementException as e:
+            return
 
         # Szczegóły ogłoszenia
         elem = elementsByXpath("/html/body/div[1]/main/div[2]/div[2]/div[1]/div")
@@ -113,18 +116,25 @@ def daneOgloszen(test):
     if test:
         dane_testowe = [
             {
-                "Nazwa": "Barska",
-                "Link": "https://www.otodom.pl/pl/oferta/stara-ochota-kamienica-54-m2-2-pok-ID4hFG3",
+                "Nazwa": "Chotomowska",
+                "Link": "https://www.otodom.pl/pl/oferta/stara-ochota-kamienica-cisza-zielen-od-zaraz-ID4hU5f",
             },
-            {
-                "Nazwa": "Jankowska",
-                "Link": "https://www.otodom.pl/pl/oferta/mieszkanie-3-pokojowe-ochota-ul-jankowska-ID4hj5U",
-            },
-            #  {'Nazwa': 'TEST2', 'Link': 'https://www.otodom.pl/pl/oferta/nowe-mieszk-4pok-z-balkonem-i-tarasem-przy-lesie-ID49nnI'}
-            {
-                "Nazwa": "TEST2",
-                "Link": "https://www.otodom.pl/pl/oferta/mieszkanie-45-30-m-warszawa-ID4huua",
-            },
+            # {
+            #     "Nazwa": "Barska",
+            #     "Link": "https://www.otodom.pl/pl/oferta/stara-ochota-kamienica-54-m2-2-pok-ID4hFG3",
+            # },
+            # {
+            #     "Nazwa": "Jankowska",
+            #     "Link": "https://www.otodom.pl/pl/oferta/mieszkanie-3-pokojowe-ochota-ul-jankowska-ID4hj5U",
+            # },
+            # {
+            #     "Nazwa": "TEST2",
+            #     "Link": "https://www.otodom.pl/pl/oferta/nowe-mieszk-4pok-z-balkonem-i-tarasem-przy-lesie-ID49nnI",
+            # },
+            # {
+            #     "Nazwa": "TEST2",
+            #     "Link": "https://www.otodom.pl/pl/oferta/mieszkanie-45-30-m-warszawa-ID4huua",
+            # },
         ]
         return dane_testowe
     else:
--- webdriver.py
+++ webdriver.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 CHROME = "./webdriver/chromedriver.exe"
 EDGE = "./webdriver/msedgedriver.exe"
