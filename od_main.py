65d7df338e012b08eb85193e75223152295a1fbe
helsing85 <helsing85@gmail.com>
2022-11-11 21:59:02 +0100

    Dane testowe w oddzielnym pliku.

--- od_main.py
+++ od_main.py
@@ -1,6 +1,7 @@
 from asyncio.windows_events import NULL
 import pandas
 import od_functions
+import od_test
 from od_webdriver import *
 
 PLIK_DANE = "oto-dom.xlsx"
@@ -121,28 +122,7 @@ def logowanie(tekst, plik):
 
 def daneOgloszen(test):
     if test:
-        dane_testowe = [
-            {
-                "Nazwa": "Chotomowska",
-                "Link": "https://www.otodom.pl/pl/oferta/stara-ochota-kamienica-cisza-zielen-od-zaraz-ID4hU5f",
-            },
-            # {
-            #     "Nazwa": "Barska",
-            #     "Link": "https://www.otodom.pl/pl/oferta/stara-ochota-kamienica-54-m2-2-pok-ID4hFG3",
-            # },
-            # {
-            #     "Nazwa": "Jankowska",
-            #     "Link": "https://www.otodom.pl/pl/oferta/mieszkanie-3-pokojowe-ochota-ul-jankowska-ID4hj5U",
-            # },
-            # {
-            #     "Nazwa": "TEST2",
-            #     "Link": "https://www.otodom.pl/pl/oferta/nowe-mieszk-4pok-z-balkonem-i-tarasem-przy-lesie-ID49nnI",
-            # },
-            # {
-            #     "Nazwa": "TEST2",
-            #     "Link": "https://www.otodom.pl/pl/oferta/mieszkanie-45-30-m-warszawa-ID4huua",
-            # },
-        ]
+        dane_testowe = od_test.dane_testowe
         return dane_testowe
     else:
         return od_functions.readLinksFromExcel(PLIK_DANE)
---
+++ od_test.py
@@ -0,0 +1,22 @@
+dane_testowe = [
+    {
+        "Nazwa": "Chotomowska",
+        "Link": "https://www.otodom.pl/pl/oferta/stara-ochota-kamienica-cisza-zielen-od-zaraz-ID4hU5f",
+    },
+    # {
+    #     "Nazwa": "Barska",
+    #     "Link": "https://www.otodom.pl/pl/oferta/stara-ochota-kamienica-54-m2-2-pok-ID4hFG3",
+    # },
+    # {
+    #     "Nazwa": "Jankowska",
+    #     "Link": "https://www.otodom.pl/pl/oferta/mieszkanie-3-pokojowe-ochota-ul-jankowska-ID4hj5U",
+    # },
+    # {
+    #     "Nazwa": "TEST2",
+    #     "Link": "https://www.otodom.pl/pl/oferta/nowe-mieszk-4pok-z-balkonem-i-tarasem-przy-lesie-ID49nnI",
+    # },
+    # {
+    #     "Nazwa": "TEST2",
+    #     "Link": "https://www.otodom.pl/pl/oferta/mieszkanie-45-30-m-warszawa-ID4huua",
+    # },
+]
